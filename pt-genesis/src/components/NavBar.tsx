988a14a5905ccb01b634e291199ee9b87206d059
JorgeCab2711 <60406863+JorgeCab2711@users.noreply.github.com>
2024-07-31 14:10:09 -0600

    finished nav bar

--- pt-genesis/src/components/NavBar.tsx
+++ pt-genesis/src/components/NavBar.tsx
@@ -3,9 +3,12 @@ import { Search, ShoppingCart, AlignRight } from 'lucide-react';
 
 const NavBar: React.FC = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-  // No media querys para mejor manejo de codigo, tailwind se lo haria ver muy desordenado
+  // No media querys para mejor manejo de codigo, tailwind se lo haria ver muy desordenado :/
+
+  // checkear el tamano de la pantalla
   useEffect(() => {
     const handleResize = () => {
+      // si la pantalla es pequena entonces setear a mobil
       setIsMobile(window.innerWidth <= 768);
     };
 
@@ -14,18 +17,19 @@ const NavBar: React.FC = () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
-
+  
+  // Renderizado condicional dependiendo del tamano de la pantalla
   return isMobile ? (
-    <div className='bg-red-500 h-fit w-auto m-3 rounded-md p-3'>
+    <div className='shadow-black h-fit w-auto m-3 rounded-md p-3'>
       <div className='flex justify-between items-center h-fit'>
-        <p className='pl-10 font-serif text-xl'>Litigade</p>
+        <p className='pl-10 font-serif text-3xl text-blue-950'>Litigade</p>
         <div className='flex gap-5 p-3'>
           <Search className='hover:cursor-pointer' />
           <ShoppingCart className='hover:cursor-pointer' />
           <AlignRight className='hover:cursor-pointer' />
         </div>
       </div>
-      <div className='grid place-items-center text-2xl p-2 bg-slate-400'>
+      <div className='grid place-items-center text-2xl p-2 gap-5'>
         <a href="">Home</a>
         <a href="">Product</a>
         <a href="">Pricing</a>
@@ -33,9 +37,9 @@ const NavBar: React.FC = () => {
       </div>
     </div>
   ) : (
-    <div className='bg-red-500 h-16 w-auto m-3 rounded-md p-2 flex justify-between items-center'>
-      <p className='pl-20 font-serif text-xl'>Litigade</p>
-      <div className='flex gap-10'>
+    <div className=' h-16 w-auto m-3 rounded-md p-2 flex justify-between items-center'>
+      <p className='pl-20 font-serif text-2xl'>Litigade</p>
+      <div className='flex gap-10 text-xl'>
         <a href="">Home</a>
         <a href="">Product</a>
         <a href="">Pricing</a>
