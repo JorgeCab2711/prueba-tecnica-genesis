974f0741160e691c3550fc6ef9f63f84f99bc0c2
JorgeCab2711 <60406863+JorgeCab2711@users.noreply.github.com>
2024-08-01 18:06:05 -0600

    Update configuration for GitHub Pages deployment

--- pt-genesis/astro.config.mjs
+++ pt-genesis/astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://JorgeCab2711.github.io',
-  base: 'prueba-tecnica-genesis',
+  site: 'https://JorgeCab2711.github.io/prueba-tecnica-genesis',
+  base: '/prueba-tecnica-genesis/',
   output: 'static',
   build: {
     outDir: 'dist',
